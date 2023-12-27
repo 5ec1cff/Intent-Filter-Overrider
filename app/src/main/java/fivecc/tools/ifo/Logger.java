7538df4b0e4f3c9265aaa96b2bc96acaff403eb4
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2023-12-27 13:52:14 +0800

    log to Xposed

--- app/src/main/java/fivecc/tools/ifo/Logger.java
+++ app/src/main/java/fivecc/tools/ifo/Logger.java
@@ -1,27 +1,28 @@
 package fivecc.tools.ifo;
 
-import android.util.Log;
+import de.robv.android.xposed.XposedBridge;
 
 public class Logger {
-    private static final String TAG = "FIVECC-IFO";
+    private static final String TAG = "IFO";
 
     public static void d(String text) {
-        // XposedBridge.log("[" + TAG + "] " + text);
-        Log.d(TAG, text);
+        XposedBridge.log("[" + TAG + "][D] " + text);
+        // Log.d(TAG, text);
     }
 
     public static void e(String text, Throwable t) {
-        // XposedBridge.log("[" + TAG + "] " + text);
-        // XposedBridge.log(t);
-        Log.e(TAG, text, t);
+        XposedBridge.log("[" + TAG + "][E] " + text);
+        XposedBridge.log(t);
+        // Log.e(TAG, text, t);
     }
 
     public static void e(String text) {
-        // XposedBridge.log("[" + TAG + "] " + text);
-        Log.e(TAG, text);
+        XposedBridge.log("[" + TAG + "][E] " + text);
+        // Log.e(TAG, text);
     }
 
     public static void w(String text) {
-        Log.w(TAG, text);
+        XposedBridge.log("[" + TAG + "][W] " + text);
+        // Log.w(TAG, text);
     }
 }

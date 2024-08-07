006a24533a85b152e9f4b6cf7e0e8a2949e7a529
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2024-08-07 18:38:11 +0800

    configure signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,16 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
 }
+
+val keystorePropertiesFile: File = rootProject.file("keystore.properties")
+val keystoreProperties = if (keystorePropertiesFile.exists() && keystorePropertiesFile.isFile) {
+    Properties().apply {
+        load(FileInputStream(keystorePropertiesFile))
+    }
+} else null
 
 android {
     namespace = "io.github.a13e300.tools.ifo"
@@ -12,6 +22,18 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        setProperty("archivesBaseName", "IFO-$versionName-$versionCode")
+    }
+
+    signingConfigs {
+        if (keystoreProperties != null) {
+            create("release") {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+            }
+        }
     }
 
     buildTypes {
@@ -20,6 +42,11 @@ android {
             isMinifyEnabled = false // true
             // isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            val releaseSig = signingConfigs.findByName("release")
+            signingConfig = if (releaseSig != null) releaseSig else {
+                println("use debug signing config")
+                signingConfigs["debug"]
+            }
         }
     }
     compileOptions {
